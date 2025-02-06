9538e9cacc900f0ee68bae0047a1c45d16905c69
Afonso <Afonsod706@gmail.com>
2025-02-06 19:26:55 +0000

    atualizado base

--- Ultimate-TicTacToe/vite.config.js
+++ Ultimate-TicTacToe/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ultimate-tic-tac-toe/',
+  base: '/UltimateTicTacToe/',
   plugins: [react()],
 })
 
